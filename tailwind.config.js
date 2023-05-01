6f041c8e34258588aa13943cc79afafbeddcb8da
mertyldrr <mertyildirir14@gmail.com>
2023-05-01 21:41:43 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+};
