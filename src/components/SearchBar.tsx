9ad6feb87bc5177a725913e7cfdfb3a065074c54
mertyldrr <mertyildirir14@gmail.com>
2023-05-02 00:41:56 +0200

    add search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
-export const SearchBar = () => {
+type Props = {
+  fetchWeather: (cityName: string, airQuality?: string) => Promise<void>;
+};
+
+export const SearchBar = ({ fetchWeather }: Props) => {
+  const [searchText, setSearchText] = useState<string>("");
+  const updateSearchText = (text: string) => {
+    setSearchText(text);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    await fetchWeather(searchText);
+  };
   return (
-    <form className="flex justify-center">
+    <form className="flex justify-center" onSubmit={handleSubmit}>
       <div className="w-full h-16 relative rounded-2xl">
         <input
           type="text"
-          id="simple-search"
+          onChange={(e) => updateSearchText(e.target.value)}
+          value={searchText}
           className=" font-poppins text-gray-400 pl-5 w-full h-full text-md rounded-lg bg-gray-700 placeholder:text-white "
           placeholder="Search..."
           required
         />
-        <FaSearch
-          className="absolute opacity-90 right-0 top-4 mr-3 p-2 select-none bg-gray-100 rounded-2xl"
-          size={36}
-        />
+        <button>
+          <FaSearch
+            className="absolute opacity-90 right-0 top-4 mr-3 p-2 select-none bg-gray-100 rounded-2xl"
+            size={36}
+          />
+        </button>
       </div>
     </form>
   );
