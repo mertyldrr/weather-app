10227cd5ab859f99aa1ef1af87e3804502032fbf
mertyldrr <mertyildirir14@gmail.com>
2023-05-03 23:07:07 +0200

    add tabs to select specific day

---
+++ src/components/DayTabs.tsx
@@ -0,0 +1,24 @@
+import { useTabStore, useWeatherStore } from "../store/store";
+import { formatDay } from "../utils/utils";
+
+export const DayTabs = () => {
+  const dayActiveTab = useTabStore((state) => state.dayActiveTab);
+  const updateForecastDayTab = useTabStore(
+    (state) => state.updateForecastDayTab
+  );
+  const forecastData = useWeatherStore((state) => state.forecastWeather);
+
+  return (
+    <div className="tabs flex justify-around">
+      {forecastData?.map((forecastDay, index) => (
+        <button
+          key={forecastDay.date}
+          className={`tab p-0 ${dayActiveTab === index ? "tab-active" : ""}`}
+          onClick={() => updateForecastDayTab(index)}
+        >
+          <p>{formatDay(forecastDay.date)}</p>
+        </button>
+      ))}
+    </div>
+  );
+};
