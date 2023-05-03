2d529436c8c3f2b254fcee31dbad88782c862079
mertyldrr <mertyildirir14@gmail.com>
2023-05-03 23:14:36 +0200

    add data types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type WeatherData = {
+export type CurrentWeatherData = {
   name: string;
   country: string;
   localTime: string;
@@ -11,9 +11,41 @@ export type WeatherData = {
   cloudCoverage: number;
 };
 
+export type ForecastDay = {
+  date: string;
+  maxtemp_f: number;
+  mintemp_f: number;
+  maxtemp_c: number;
+  mintemp_c: number;
+  condition: Condition;
+  hour: Array<any>;
+};
+
+export type Hour = {
+  timestamp: number;
+  temp_c: number;
+  temp_f: number;
+  day: number;
+  condition: Condition;
+};
+
 export type Condition = {
   text: string;
   icon: string;
 };
 
+export enum WeatherOptionalParams {
+  Yes = "yes",
+  No = "no",
+}
+
+export enum WeatherChoice {
+  Current = 1,
+  Forecast = 2,
+}
+export enum DayChoice {
+  Current = 0,
+  Tomorrow = 1,
+  DayAfterTmrw = 2,
+}
 export const CELSIUS_SYMBOL = "\u00B0C";
