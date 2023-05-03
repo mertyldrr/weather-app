b43930f36c450fe16a4737f6653d8c8218971e7a
mertyldrr <mertyildirir14@gmail.com>
2023-05-03 23:14:18 +0200

    add 3-days forecast component

---
+++ src/components/Forecast.tsx
@@ -0,0 +1,31 @@
+import { useTabStore, useWeatherStore } from "../store/store";
+import { CELSIUS_SYMBOL } from "../types";
+import { formatHour } from "../utils/utils";
+
+export const Forecast = () => {
+  const forecastData = useWeatherStore((state) => state.forecastWeather);
+  const activeDay = useTabStore((state) => state.dayActiveTab);
+  return (
+    <div>
+      <div className="flex overflow-x-auto scrollbar-none space-x-8 py-2">
+        {forecastData![activeDay].hour.map((hour) => (
+          <div className="flex flex-col" key={hour.time_epoch}>
+            <div>{formatHour(hour.time_epoch)} </div>
+            <img
+              className="w-8"
+              src={hour.condition.icon}
+              alt={hour.condition.text}
+            />
+          </div>
+        ))}
+      </div>
+
+      <p className="font-poppins font text-2xl text-white mt-6">
+        {`Max temp: ${forecastData![activeDay].maxtemp_c} ${CELSIUS_SYMBOL}`}
+      </p>
+      <p className="font-poppins font-normal text-2xl text-white mt-4">
+        {`Min temp: ${forecastData![activeDay].mintemp_c} ${CELSIUS_SYMBOL}`}
+      </p>
+    </div>
+  );
+};
