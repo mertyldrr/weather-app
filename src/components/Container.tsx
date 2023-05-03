4158c4b55fec66615482173cc509625c6c181b7f
mertyldrr <mertyildirir14@gmail.com>
2023-05-03 23:14:01 +0200

    fetch weather info based on active tab

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,55 +1,113 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { WeatherData } from "../types";
-import { WeatherInformation } from "./WeatherInformation";
+import { useCallback, useEffect } from "react";
+import {
+  CurrentWeatherData,
+  ForecastDay,
+  WeatherChoice,
+  WeatherOptionalParams,
+} from "../types";
+import { CurrentWeather } from "./CurrentWeather";
 import { SearchBar } from "./SearchBar";
+import { WeatherTabs } from "./WeatherTabs";
+import { useSearchStore, useTabStore, useWeatherStore } from "../store/store";
+import { Forecast } from "./Forecast";
+import { DayTabs } from "./DayTabs";
 
 export const Container = () => {
-  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const handleWeatherData = (weatherData: WeatherData) => {
-    setWeatherData(weatherData);
-  };
+  const lastSearch = useSearchStore((state) => state.lastSearch);
+  const activeTab = useTabStore((state) => state.forecastTypeActiveTab);
+  const {
+    currentWeather,
+    forecastWeather,
+    updateCurrentWeather,
+    updateForecastWeather,
+  } = useWeatherStore((state) => state);
 
-  const fetchWeather = async (
-    cityName: string,
-    airQuality?: string
-  ): Promise<void> => {
-    const res = await axios.get(
-      `${process.env.REACT_APP_WEATHER_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${cityName}&aqi=${airQuality}`,
-      {
-        headers: {
-          "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Origin": true,
-        },
+  const fetchCurrentWeather = useCallback(
+    async (
+      cityName: string,
+      airQuality?: WeatherOptionalParams
+    ): Promise<void> => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_WEATHER_API_URL}/current.json?key=${process.env.REACT_APP_API_KEY}&q=${cityName}&aqi=${airQuality}`
+      );
+      const { name, country, localTime } = res.data.location;
+      const { temp_c, temp_f, condition, wind_mph, wind_kph, humidity, cloud } =
+        res.data.current;
+      const mergedObject: CurrentWeatherData = {
+        name,
+        country,
+        localTime,
+        temp_c,
+        temp_f,
+        condition,
+        wind_mph,
+        wind_kph,
+        humidity,
+        cloudCoverage: cloud,
+      };
+      updateCurrentWeather(mergedObject);
+    },
+    [updateCurrentWeather]
+  );
+
+  const fetchWeatherForecast = useCallback(
+    async (
+      cityName: string,
+      days = 3,
+      airQuality?: WeatherOptionalParams,
+      weatherAlerts?: WeatherOptionalParams
+    ): Promise<void> => {
+      const res = await axios.get(
+        `${process.env.REACT_APP_WEATHER_API_URL}/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${cityName}&days=${days}&aqi=${airQuality}&alerts=${weatherAlerts}`
+      );
+      let forecastArray: ForecastDay[] = [];
+      const { forecast } = res.data;
+      const { forecastday } = forecast;
+      for (let i = 0; i < days; i++) {
+        const { date, day, hour } = forecastday[i];
+        const { maxtemp_f, mintemp_f, maxtemp_c, mintemp_c, condition } = day;
+
+        let forecast: ForecastDay = {
+          date,
+          maxtemp_f,
+          mintemp_f,
+          maxtemp_c,
+          mintemp_c,
+          condition,
+          hour,
+        };
+        forecastArray.push(forecast);
       }
-    );
-    const { name, country, localTime } = res.data.location;
-    const { temp_c, temp_f, condition, wind_mph, wind_kph, humidity, cloud } =
-      res.data.current;
-    const mergedObject: WeatherData = {
-      name,
-      country,
-      localTime,
-      temp_c,
-      temp_f,
-      condition,
-      wind_mph,
-      wind_kph,
-      humidity,
-      cloudCoverage: cloud,
-    };
-    handleWeatherData(mergedObject);
-  };
+      updateForecastWeather(forecastArray);
+    },
+    [updateForecastWeather]
+  );
+  useEffect(() => {
+    fetchCurrentWeather("Munich");
+  }, [fetchCurrentWeather]);
 
   useEffect(() => {
-    fetchWeather("Munich");
-  }, []);
+    activeTab === WeatherChoice.Current
+      ? fetchCurrentWeather(lastSearch)
+      : fetchWeatherForecast(lastSearch);
+  }, [activeTab, lastSearch, fetchCurrentWeather, fetchWeatherForecast]);
 
   return (
-    <div className="w-screen h-screen bg-gray-200 flex justify-center items-center">
-      <div className="w-1/2 h-2/5 bg-black rounded-2xl p-8">
-        <SearchBar fetchWeather={fetchWeather} />
-        {weatherData && <WeatherInformation weatherData={weatherData} />}
+    <div className="w-screen h-screen bg-munich bg-cover flex justify-center items-center">
+      <div className="w-1/2 h-2/5 bg-black rounded-2xl p-8 opacity-70">
+        <SearchBar fetchCurrentWeather={fetchCurrentWeather} />
+        <div>
+          <WeatherTabs />
+          {activeTab === WeatherChoice.Current
+            ? currentWeather && <CurrentWeather />
+            : forecastWeather && (
+                <div>
+                  <DayTabs />
+                  <Forecast />
+                </div>
+              )}
+        </div>
       </div>
     </div>
   );
