70b2887d3f45f9773cdfb710ab57e94c4c333495
mertyldrr <mertyildirir14@gmail.com>
2023-05-03 23:06:22 +0200

    replace weatherInfo component with currentWeather

---
+++ src/components/CurrentWeather.tsx
@@ -0,0 +1,32 @@
+import { useWeatherStore } from "../store/store";
+import { CELSIUS_SYMBOL } from "../types";
+
+export const CurrentWeather = () => {
+  const currentWeather = useWeatherStore((state) => state.currentWeather!);
+  return (
+    <div className="flex flex-col mt-4">
+      <p className="font-poppins text-4xl text-white">
+        Weather in {currentWeather.name}
+      </p>
+      <p className="font-poppins font-semibold text-6xl text-white">
+        {`${currentWeather.temp_c} ${CELSIUS_SYMBOL}`}
+      </p>
+      <div className="flex flex-row items-center">
+        <img
+          className="w-12"
+          src={currentWeather.condition.icon}
+          alt={currentWeather.condition.text}
+        />
+        <p className="font-poppins text-xl text-white">
+          {currentWeather.condition.text}
+        </p>
+      </div>
+      <p className="font-poppins font-light text-lg text-white">
+        {`Humidity: ${currentWeather.humidity}%`}
+      </p>
+      <p className="font-poppins font-light text-lg text-white">
+        {`Wind speed: ${currentWeather.temp_c} km/hr`}
+      </p>
+    </div>
+  );
+};
